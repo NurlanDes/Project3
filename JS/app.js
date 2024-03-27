4039ef0b0804b3315b0435a2485d23caf1e1f8ab
NurlanDes <nduishobekov@gmail.com>
2024-03-27 16:58:53 +0600

    Homework 47

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+// console.log("hello world");
+// console.log(1 + 2); //number
+// console.log("1"+"2"); //string
+// console.log("hello" + " " + "world");
+
+// var myVar = 10;
+
+// var b = myVar * 9;
+
+// console.log(b);
+
+console.log("hello world my name is" + "my Name")
